5b2a9d688e85daa9e9b2ca04f9b676c15a7e6b62
MichaelVozzo <mvozzo05@gmail.com>
2025-04-11 11:34:06 +0930

    Adjust grid gap

--- components/TechnologyBadges.tsx
+++ components/TechnologyBadges.tsx
@@ -3,7 +3,7 @@ import TechBadge from "@/components/TechBadge";
 
 const TechnologyBadges = ({ technologies }: { technologies: any[] }) => {
   return (
-    <div className="grid grid-cols-[repeat(auto-fit,minmax(100px,1fr))] gap-y-8 gap-x-12 md:gap-y-12 md:gap-x-[4rem]">
+    <div className="grid grid-cols-[repeat(auto-fit,minmax(100px,1fr))] gap-y-8 gap-x-6 md:gap-y-12 md:gap-x-[4rem]">
       {technologies.map((tech) => (
         <TechBadge
           key={tech.name}
