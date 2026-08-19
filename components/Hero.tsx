11a273192e437b29181a9aad7429f50e93f24297
Michael Vozzo <michael@argon.com.au>
2026-08-19 20:53:44 +0930

    Fixed npm vulnerabilities

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,26 +1,30 @@
 import Link from "next/link";
 import Image from "next/image";
-import profilePic2 from "/public/images/mv-home-shot-2.webp";
-import linkedinLogo from "/public/images/techologies/linkedin.svg";
-import githubLight from "/public/images/techologies/Github_light.svg";
-import githubDark from "/public/images/techologies/Github_dark.svg";
 import { ArrowRight } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Container, Section } from "@/components/Layout";
+
+const profilePic2 = "/images/mv-home-shot-2.webp";
+const linkedinLogo = "/images/techologies/linkedin.svg";
+const githubLight = "/images/techologies/Github_light.svg";
+const githubDark = "/images/techologies/Github_dark.svg";
 
 export default function Hero() {
 	return (
 		<Section>
 			<Container className="overflow-x-clip lg:overflow-visible">
-				<div className="grid grid-cols-1 md:grid-cols-2 gap-12 md:gap-24 items-center">
+				<div className="grid grid-cols-1 items-center gap-12 md:grid-cols-2 md:gap-24">
 					<div className="flex flex-col space-y-4">
-						<h1 className="md:text-xl font-semibold relative rainbow-frame self-start">Full stack web developer in Adelaide</h1>
+						<h1 className="relative self-start rainbow-frame font-semibold md:text-xl">Full stack web developer in Adelaide</h1>
+
 						<p className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">Hi, I&apos;m Michael!</p>
-						<p className="md:text-lg text-pretty max-w-[600px]">
+
+						<p className="max-w-[600px] text-pretty md:text-lg">
 							I specialise in building high-performance, scalable, and user-centric web solutions, leveraging technologies such as
 							React, TypeScript, Next.JS and WordPress.
 						</p>
-						<div className="flex flex-col sm:flex-row gap-4 pt-4">
+
+						<div className="flex flex-col gap-4 pt-4 sm:flex-row">
 							<Button asChild size="lg">
 								<Link href="/projects">
 									View My Work
@@ -28,43 +32,26 @@ export default function Hero() {
 								</Link>
 							</Button>
 						</div>
+
 						<div className="flex flex-row gap-4 pt-4">
 							<a href="https://www.linkedin.com/in/mgxhtt-8bbb354b" target="_blank" rel="noopener noreferrer">
-								<Image
-									src={linkedinLogo}
-									alt="Michael's Linkedin Profile Link"
-									className="object-contain"
-									width={30}
-									height={30}
-									priority
-								/>
+								<Image src={linkedinLogo} alt="Michael's LinkedIn profile" width={30} height={30} priority />
 							</a>
+
 							<a href="https://github.com/MichaelVozzo" className="dark:hidden" target="_blank" rel="noopener noreferrer">
-								<Image
-									src={githubLight}
-									alt="Michael's Github Profile Link"
-									className="object-contain dark:hidden"
-									width={30}
-									height={30}
-									priority
-								/>
+								<Image src={githubLight} alt="Michael's GitHub profile" width={30} height={30} priority />
 							</a>
+
 							<a href="https://github.com/MichaelVozzo" className="hidden dark:block" target="_blank" rel="noopener noreferrer">
-								<Image
-									src={githubDark}
-									alt="Michael's Github Profile Link"
-									className="object-contain hidden dark:block"
-									width={30}
-									height={30}
-									priority
-								/>
+								<Image src={githubDark} alt="Michael's GitHub profile" width={30} height={30} priority />
 							</a>
 						</div>
 					</div>
-					<div className="relative flex flex-col justify-center items-center">
-						<div className="oval-background"></div>
-						<div className="relative w-[80%] h-[400px] md:h-[600px] overflow-hidden z-10">
-							<Image src={profilePic2} alt="Michael Vozzo web developer" fill className="object-contain" priority />
+
+					<div className="relative flex flex-col items-center justify-center">
+						<div className="oval-background" />
+						<div className="relative z-10 h-[400px] w-[80%] overflow-hidden md:h-[600px]">
+							<Image src={profilePic2} alt="Michael Vozzo, web developer" fill className="object-contain" priority />
 						</div>
 					</div>
 				</div>
--- data/technologies.ts
+++ data/technologies.ts
@@ -1,103 +1,88 @@
-import ReactLight from "/public/images/techologies/React_light.svg";
-import ReactDark from "/public/images/techologies/React_dark.svg";
-import NextJsLight from "/public/images/techologies/Next_js_light.svg";
-import NextJsDark from "/public/images/techologies/Next_js_dark.svg";
-import PhpLight from "/public/images/techologies/Php_light.svg";
-import PhpDark from "/public/images/techologies/Php_dark.svg";
-import TypeScript from "/public/images/techologies/typescript.svg";
-import Postgres from "/public/images/techologies/postgresql.svg";
-import WordPress from "/public/images/techologies/wordpress.svg";
-import MySQL from "/public/images/techologies/mysql.svg";
-import TailwindCSS from "/public/images/techologies/tailwindcss.svg";
-import GraphQL from "/public/images/techologies/graphql.svg";
-import Docker from "/public/images/techologies/docker.svg";
-import Cloudflare from "/public/images/techologies/cloudflare.svg";
-import Git from "/public/images/techologies/git.svg";
+const images = "/images/techologies/";
 
-// Logos Grid Data Array
 export const technologies = [
-  {
-    name: "React",
-    imageLight: ReactLight,
-    imageDark: ReactDark,
-    width: 40,
-    height: 40,
-  },
-  {
-    name: "Next.js",
-    imageLight: NextJsLight,
-    imageDark: NextJsDark,
-    width: 85,
-    height: 40,
-  },
-  {
-    name: "TypeScript",
-    imageLight: TypeScript,
-    imageDark: "",
-    width: 40,
-    height: 40,
-  },
-  {
-    name: "PostgreSQL",
-    imageLight: Postgres,
-    imageDark: "",
-    width: 40,
-    height: 40,
-  },
-  {
-    name: "WordPress",
-    imageLight: WordPress,
-    imageDark: "",
-    width: 40,
-    height: 40,
-  },
-  {
-    name: "PHP",
-    imageLight: PhpLight,
-    imageDark: PhpDark,
-    width: 60,
-    height: 40,
-  },
-  {
-    name: "mySQL",
-    imageLight: MySQL,
-    imageDark: "",
-    width: 40,
-    height: 40,
-  },
-  {
-    name: "Tailwind CSS",
-    imageLight: TailwindCSS,
-    imageDark: "",
-    width: 40,
-    height: 40,
-  },
-  {
-    name: "GraphQL",
-    imageLight: GraphQL,
-    imageDark: "",
-    width: 40,
-    height: 40,
-  },
-  {
-    name: "Docker",
-    imageLight: Docker,
-    imageDark: "",
-    width: 40,
-    height: 40,
-  },
-  {
-    name: "Cloudflare",
-    imageLight: Cloudflare,
-    imageDark: "",
-    width: 50,
-    height: 40,
-  },
-  {
-    name: "Git",
-    imageLight: Git,
-    imageDark: "",
-    width: 40,
-    height: 40,
-  },
+	{
+		name: "React",
+		imageLight: `${images}React_light.svg`,
+		imageDark: `${images}React_dark.svg`,
+		width: 40,
+		height: 40,
+	},
+	{
+		name: "Next.js",
+		imageLight: `${images}Next_js_light.svg`,
+		imageDark: `${images}Next_js_dark.svg`,
+		width: 85,
+		height: 40,
+	},
+	{
+		name: "TypeScript",
+		imageLight: `${images}typescript.svg`,
+		imageDark: "",
+		width: 40,
+		height: 40,
+	},
+	{
+		name: "PostgreSQL",
+		imageLight: `${images}postgresql.svg`,
+		imageDark: "",
+		width: 40,
+		height: 40,
+	},
+	{
+		name: "WordPress",
+		imageLight: `${images}wordpress.svg`,
+		imageDark: "",
+		width: 40,
+		height: 40,
+	},
+	{
+		name: "PHP",
+		imageLight: `${images}Php_light.svg`,
+		imageDark: `${images}Php_dark.svg`,
+		width: 60,
+		height: 40,
+	},
+	{
+		name: "mySQL",
+		imageLight: `${images}mysql.svg`,
+		imageDark: "",
+		width: 40,
+		height: 40,
+	},
+	{
+		name: "Tailwind CSS",
+		imageLight: `${images}tailwindcss.svg`,
+		imageDark: "",
+		width: 40,
+		height: 40,
+	},
+	{
+		name: "GraphQL",
+		imageLight: `${images}graphql.svg`,
+		imageDark: "",
+		width: 40,
+		height: 40,
+	},
+	{
+		name: "Docker",
+		imageLight: `${images}docker.svg`,
+		imageDark: "",
+		width: 40,
+		height: 40,
+	},
+	{
+		name: "Cloudflare",
+		imageLight: `${images}cloudflare.svg`,
+		imageDark: "",
+		width: 50,
+		height: 40,
+	},
+	{
+		name: "Git",
+		imageLight: `${images}git.svg`,
+		imageDark: "",
+		width: 40,
+		height: 40,
+	},
 ];
